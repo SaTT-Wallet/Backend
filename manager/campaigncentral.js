a368ea0186ea95d8e96c1b99916effea508bbebc
geoffreymoya <geoffreym.moya@gmail.com>
2020-12-30 18:27:53 +0100

    debug

--- manager/campaigncentral.js
+++ manager/campaigncentral.js
@@ -179,7 +179,7 @@ module.exports = async function (app) {
 					{
 						typeSNindex = parseInt(prom.typeSN)*3;
 						console.log(stats.likes,cmp.ratios[typeSNindex-3])
-						var gains = (new BN( stats.likes)).mul(new BN(cmp.ratios[typeSNindex-3]));
+						/*var gains = (new BN( stats.likes)).mul(new BN(cmp.ratios[typeSNindex-3]));
 
 						gains = gains.add( (new BN( stats.shares)).mul( new BN(cmp.ratios[typeSNindex-2])));
 						gains = gains.add( (new BN( stats.views)).mul( new BN(cmp.ratios[typeSNindex-1])));
@@ -200,7 +200,7 @@ module.exports = async function (app) {
 
 						app.web3.eth.accounts.wallet.decrypt([app.config.sattReserveKs], app.config.SattReservePass);
 						var receipt = await app.erc20.transfer(cmp.token,prom.influencer,topay.toString(),{address:app.config.SattReserve})
-						resolve({transactionHash:receipt.transactionHash,idProm:idProm,to:prom.influencer,amount:topay.toString()})
+						resolve({transactionHash:receipt.transactionHash,idProm:idProm,to:prom.influencer,amount:topay.toString()})*/
 					}
 
 				})
