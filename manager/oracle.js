beb9d6a42b4d4a0004782fa011fe0332620cf170
geoffreymo <geoffreym.moya@gmail.com>
2020-12-30 15:32:03 +0100

    log body

--- manager/oracle.js
+++ manager/oracle.js
@@ -1,24 +1,24 @@
 module.exports = async function (app) {
-	
+
 	var request = require('request');
 	var rp = require('request-promise');
 	var fs = require("fs");
 	var Twitter = require('twitter');
 	var jsdomlib = require("jsdom");
 	var jsdom = jsdomlib.JSDOM;
-	
+
 	var tweet = new Twitter({
 	  consumer_key: app.config.twitter.consumer_key,
 	  consumer_secret: app.config.twitter.consumer_secret,
 	  access_token_key: app.config.twitter.access_token_key,
 	  access_token_secret: app.config.twitter.access_token_secret
 	});
-	
+
 	var oracleManager = {};
-	
+
 	var campaignKeystore = fs.readFileSync(app.config.campaignWalletPath,'utf8');
 	app.campaignWallet = JSON.parse(campaignKeystore);
-	
+
 	/*oracleManager.parseSign = function (data) {
 		data = data.slice(2);
 		var ret = {};
@@ -27,17 +27,17 @@ module.exports = async function (app) {
 		ret.v = app.web3.utils.hexToNumber(data.slice(128, 130));
 		return ret;
 	}*/
-	
+
 	oracleManager.facebookAlt = async function (pageName,idPost) {
 		return new Promise(async (resolve, reject) => {
 			//app.FB.api(pageName,{ access_token: "" }, async function (res) {
-				
+
 				var res = await rp({uri:"https://graph.facebook.com/v3.2/"+pageName+"?access_token="+app.FB.appAccessToken,json: true});
 				console.log("pageres",res);
 				var idPage = res.id;
 				console.log("https://graph.facebook.com/v3.2/"+idPage+"_"+idPost+"?fields=shares,likes.summary(true)&access_token="+app.FB.appAccessToken);
 				var res2 = await rp({uri:"https://graph.facebook.com/v3.2/"+idPage+"_"+idPost+"?fields=shares,likes.summary(true)&access_token="+app.FB.appAccessToken,json: true});
-				
+
 				//app.FB.api(idPage+"_"+idPost+"?fields=shares,likes.summary(true)",{ access_token: app.FB.appAccessToken }, async function (res2) {
 					var shares = 0;
 					if(res2.error)
@@ -62,13 +62,13 @@ module.exports = async function (app) {
 			//})
 		})
 	};
-	
+
 	oracleManager.facebook = async function (pageName,idPost) {
 		return new Promise(async (resolve, reject) => {
-			
+
 			var res = await rp({uri:"http://geoffreymoya.hopto.org:3014/fb/page/"+pageName+"/"+idPost,json: true})
-				
-				
+
+
 					var perf = {shares:res.shares,likes:res.likes,views:0,date:Math.floor(Date.now()/1000)};
 					app.web3.eth.accounts.wallet.decrypt([app.campaignWallet], app.config.campaignOwnerPass);
 					var h1 = app.web3.utils.sha3(app.web3.eth.abi.encodeParameters(['uint8','string','string','uint64','uint64','uint64','uint64'],[1,idPost,"",perf.shares,perf.likes,perf.views,perf.date]));
@@ -78,10 +78,10 @@ module.exports = async function (app) {
 					perf.v = sign.v;
 					resolve(perf);
 				//}
-			
+
 		})
 	};
-	
+
 	oracleManager.youtube = async function (idPost) {
 		return new Promise(async (resolve, reject) => {
 			if( -1 != idPost.indexOf("&"))
@@ -90,10 +90,11 @@ module.exports = async function (app) {
 			}
 			var body = await rp({uri:'https://www.googleapis.com/youtube/v3/videos',qs:{id:idPost,key:app.config.gdataApiKey,part:"statistics"}});
 			var res = JSON.parse(body);
+			console.log(res);
 			//await app.web3.eth.personal.unlockAccount(app.config.campaignOwner,app.config.campaignOwnerPass,app.web3.utils.toHex(15000));
 			//app.web3.eth.accounts.wallet.decrypt([app.campaignWallet], app.config.campaignOwnerPass);
 			var perf = {shares:res.items[0].statistics.commentCount,likes:res.items[0].statistics.likeCount,views:res.items[0].statistics.viewCount,date:Math.floor(Date.now()/1000)};
-			
+
 		   /* var h1 = app.web3.utils.sha3(app.web3.eth.abi.encodeParameters(['uint8','string','string','uint64','uint64','uint64','uint64'],[2,idPost,"",perf.shares,perf.likes,perf.views,perf.date]));
 			var sign = await app.web3.eth.sign(h1,app.config.campaignOwner);
 			perf.r = sign.r;
@@ -101,14 +102,14 @@ module.exports = async function (app) {
 			perf.v = sign.v;*/
 			resolve(perf);
 		})
-		
+
 	};
-	
+
 	oracleManager.instagram = async function (idPost) {
 		return new Promise(async (resolve, reject) => {
-			
+
 			var perf = await rp({uri:"http://geoffreymoya.hopto.org:3014/insta/"+idPost,json: true})
-			
+
 			/*var body = await rp({uri:"https://www.instagram.com/p/"+idPost});
 			var part = body.split('<meta property="og:description" content="')[1];
 			var part2 = part.split(' ');
@@ -127,22 +128,22 @@ module.exports = async function (app) {
 			perf.v = sign.v;
 			resolve(perf);*/
 		})
-		
+
 	};
-	
+
 	oracleManager.twitter = async function (userName,idPost) {
-		
+
 		return new Promise(async (resolve, reject) => {
-			
-			
+
+
 			/*var body = await rp({uri:"https://twitter.com/"+userName+"/status/"+idPost});
 			var parts = body.split('data-tweet-stat-count=');
 			var partT = parts[1];
 			var partL = parts[2];*/
 			var res = await tweet.get('statuses/show',{id:idPost});
-			
+
 			var perf = {shares:res.retweet_count,likes:res.favorite_count,views:0,date:Math.floor(Date.now()/1000)};
-			
+
 			//var perf = {shares:partT.split('"')[1],likes:partL.split('"')[1],views:0,date:Math.floor(Date.now()/1000)};
 			//app.web3.eth.accounts.wallet.decrypt([app.campaignWallet], app.config.campaignOwnerPass);
 			//var h1 = app.web3.utils.sha3(app.web3.eth.abi.encodeParameters(['uint8','string','string','uint64','uint64','uint64','uint64'],[4,idPost,userName,perf.shares,perf.likes,perf.views,perf.date]));
@@ -151,11 +152,11 @@ module.exports = async function (app) {
 			perf.r = sign.r;
 			perf.s = sign.s;
 			perf.v = sign.v;*/
-			
+
 			resolve(perf);
 		})
 	};
-	
+
 	app.oracle = oracleManager;
 	return app;
 }
