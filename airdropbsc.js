e68f138681f8a0e9f6f28030ea3df1165b149fb3
geoffrey <geoffrey.moya@gmail.com>
2021-03-17 11:11:55 +0100

    test

--- airdropbsc.js
+++ airdropbsc.js
@@ -16,8 +16,15 @@ try {
 
     const data = fs.readFileSync('./bsc.csv', 'UTF-8');
 
+    var reservePath = fs.readFileSync("./conf/steps/campaign.json",'utf8');
+    var reserveBep20 = JSON.parse(reservePath);
+
+    app.web3Bep20.eth.accounts.wallet.decrypt([reserveBep20], app.config.SattReservePass);
+
     // split the contents by new line
     const lines = data.split(/\r?\n/);
+
+      var total = new BN("0");
 
     // print all lines
     lines.forEach((line) => {
@@ -25,7 +32,15 @@ try {
         var address = parts[1];
         var amount = new BN(parseFloat(parts[3]))
         var mul18  =  new BN("1000000000000000000");
-        console.log(address,amount.mul(mul18).toString());
+        var bonus = amount.mul(mul18).div(new BN(20));
+
+
+        total = total.add(amount);
+
+        //var receipt = await app.bep20.transferBEP(address,bonus,{address:"0x"+reserveBep20.address});
+        //console.log(receipt);
+
+        console.log(address,amount.toString(),total.toString());
     });
 } catch (err) {
     console.error(err);
