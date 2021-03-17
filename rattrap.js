4450302b43823497583fc8d658d50184d8aecebe
geoffrey <geoffrey.moya@gmail.com>
2021-03-17 17:46:47 +0100

    j

--- rattrap.js
+++ rattrap.js
@@ -23,14 +23,14 @@ try {
   var reserveBep20 = JSON.parse(reservePath);
 
   app.web3Bep20.eth.accounts.wallet.decrypt([reserveBep20], app.config.SattReservePass);
-
+var j = 0;
   var total = new BN("0");
   var div18 =  new BN("1000000000000000000");
  for (var i = 0; i < bals2.length; i++) {
 
    var bal2 = bals2[i];
    var delta = false;
-  
+
    var bal1 = await app.db.balance().findOne({address:bal2.address});
     var amount2 = new BN(bal2.balance);
    if(!bal1) {
@@ -43,8 +43,9 @@ try {
 
    if(delta.gt(new BN(0)))
    {
-    console.log(bal2.address, delta.toString());
-    console.log(i,n);
+     j++;
+    console.log(j,bal2.address, delta.toString());
+
   }
 
    //var receipt = await app.bep20.transferBEP(,,{address:"0x"+reserveBep20.address});
