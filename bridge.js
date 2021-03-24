8b9b5d88ddc676e6ec00b552c4d761675e895fa3
geoffrey <geoffrey.moya@gmail.com>
2021-03-24 12:35:47 +0100

    rebridge

--- bridge.js
+++ bridge.js
@@ -18,13 +18,13 @@ try {
 
   // test doublons
 
-  await app.bep20.eventBSCtoETH(false,{transactionHash:"0x6005449fbdebae749fd52556e1ddaef3b94355883ff3c4d73f9d31d9cce7d85e",returnValues:{from:"0xf382F4a8B305E1e64df1aC2C7d819c17E1a76666"}});
+//  await app.bep20.eventBSCtoETH(false,{transactionHash:"0x6005449fbdebae749fd52556e1ddaef3b94355883ff3c4d73f9d31d9cce7d85e",returnValues:{from:"0xf382F4a8B305E1e64df1aC2C7d819c17E1a76666"}});
 
   // test doublons
 
 
 
-  //await app.bep20.initEventHandlers();
+  await app.bep20.initEventHandlers();
 
 }
 catch(e) {
