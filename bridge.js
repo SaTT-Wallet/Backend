94b6e183b3fba2efc04af4e8cd2325547f402c15
geoffrey <geoffrey.moya@gmail.com>
2021-03-24 12:29:24 +0100

    log

--- bridge.js
+++ bridge.js
@@ -18,8 +18,8 @@ try {
 
   // test doublons
   await app.bep20.eventETHtoBSC(false,{transactionHash:"0xdafd0bbef6eaebcdcbb3178191ef38c844d728a60460d8c6f67a7ccb6101b02f"});
-  await app.bep20.eventBSCtoETH(false,{transactionHash:"0xdafd0bbef6eaebcdcbb3178191ef38c844d728a60460d8c6f67a7ccb6101b02f"});
-  await app.bep20.eventBSCtoETH(false,{transactionHash:"0xdafd0bbef6eaebcdcbb3178191ef38c844d728a60460d8c6f67a7ccb6101b02f"});
+  await app.bep20.eventBSCtoETH(false,{transactionHash:"0x6005449fbdebae749fd52556e1ddaef3b94355883ff3c4d73f9d31d9cce7d85b"});
+  
   // test doublons
 
 
