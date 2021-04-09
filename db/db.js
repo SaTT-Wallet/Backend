db6ff1a8a4677ff9176cb3b9bcb496271606bad8
HamdiBenK <hamdi@atayen.us>
2021-04-09 16:27:07 +0100

    delete cover

--- db/db.js
+++ db/db.js
@@ -46,8 +46,8 @@ module.exports = async function (app) {
 			});
 		});
 	}
-
-	 var db = await mongoClient.connect("mongodb://" + app.config.mongoUser + ":" + app.config.mongoPass + "@" + app.config.mongoHost + ":" + app.config.mongoPort + "/" + app.config.mongoBase, {useNewUrlParser: true,useUnifiedTopology: true}).catch(console.log)
+	var db = await mongoClient.connect("mongodb://" + app.config.mongoHost + ":" + app.config.mongoPort + "/" + app.config.mongoBase, {useNewUrlParser: true,useUnifiedTopology: true}).catch(console.log)
+	//  var db = await mongoClient.connect("mongodb://" + app.config.mongoUser + ":" + app.config.mongoPass + "@" + app.config.mongoHost + ":" + app.config.mongoPort + "/" + app.config.mongoBase, {useNewUrlParser: true,useUnifiedTopology: true}).catch(console.log)
 	var db2 = await mongoClient.connect("mongodb://"+ app.config.mongoHost + ":" + app.config.mongoPort + "/" + app.config.mongoBaseCrm, {useNewUrlParser: true,useUnifiedTopology: true}).catch(console.log)
 
          
@@ -61,6 +61,9 @@ module.exports = async function (app) {
 
 		app.db.campaign_link = function () {
             return db.db(app.config.mongoBaseCrm).collection("campaign_link");
+        };
+		app.db.campaign_link = function () {
+            return db.db(app.config.mongoBaseCrm).collection("CampaignCover");
         };
 		app.db.user = function () {
             return db2.db(app.config.mongoBaseCrm).collection("sn_user");
