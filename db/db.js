68cd07403367d3e1249a24134b2f4c76c1f82ab9
Haythem <haythem@atayen.us>
2021-04-14 08:25:41 +0100

    accepted proms && get cover kits

--- db/db.js
+++ db/db.js
@@ -51,7 +51,6 @@ module.exports = async function (app) {
 	 var db = await mongoClient.connect("mongodb://" + app.config.mongoUser + ":" + app.config.mongoPass + "@" + app.config.mongoHost + ":" + app.config.mongoPort + "/" + app.config.mongoBase, {useNewUrlParser: true,useUnifiedTopology: true}).catch(console.log)
 	 var db2 = await mongoClient.connect("mongodb://"+ app.config.mongoHost + ":" + app.config.mongoPort + "/" + app.config.mongoBaseCrm, {useNewUrlParser: true,useUnifiedTopology: true}).catch(console.log)
 
-         
 
 		app.db.campaignCrm = function () {
             return db2.db(app.config.mongoBaseCrm).collection(app.config.campaignCollection);
@@ -65,13 +64,20 @@ module.exports = async function (app) {
 		app.db.campaign_link = function () {
             return db.db(app.config.mongoBaseCrm).collection("campaign_link");
         };
+		
+		app.db.UserLegal = function () {
+            return db.db(app.config.mongoBaseCrm).collection("user_legal");
+        };
 		app.db.campaignCover = function () {
             return db.db(app.config.mongoBaseCrm).collection("campaign_cover");
         };
+
 		app.db.user = function () {
             return db2.db(app.config.mongoBaseCrm).collection("sn_user");
         };
-
+		app.db.userFiles = function () {
+            return db.db(app.config.mongoBaseCrm).collection("user_files");
+        };
         app.db.wallet = function () {
             return db.db(app.config.mongoBase).collection(app.config.walletCollection);
         };
