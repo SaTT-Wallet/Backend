3c83062f12bcbcfa6b2f472d126aa9bb9e07b4a3
HamdiBenK <hamdi@atayen.us>
2021-04-21 09:09:42 +0100

    git list without pagination

--- express/stats.js
+++ express/stats.js
@@ -346,6 +346,89 @@ module.exports = function (app) {
 	})
 
 
+	app.get('/campaigns/list/:addr', async function(req, response) {
+		try{
+			let token = req.headers["authorization"].split(" ")[1];
+            await app.crm.auth(token);
+			var owner = req.params.addr;
+			var access_token= token
+			var campaigns = [];
+			var rescampaigns = [];
+
+			campaigns = await app.db.campaign().find({contract:{$ne : "central"},owner:owner}).toArray();
+			var campaignsCrm = [];
+			var campaignsCrmbyId = [];
+			campaignsCrm = await app.db.campaignCrm().find().toArray();
+			for (var i = 0;i<campaignsCrm.length;i++)
+			{
+				if(campaignsCrm[i].hash)
+					campaignsCrmbyId[campaignsCrm[i].hash] = campaignsCrm[i];
+			}
+			for (var i = 0;i<campaigns.length;i++)
+			{
+				var ctr = await app.campaign.getCampaignContract(campaigns[i].id);
+				if(!ctr.methods)
+				{
+					continue;
+				}
+
+				if(campaignsCrmbyId[campaigns[i].id])
+				{
+					campaigns[i].meta = campaignsCrmbyId[campaigns[i].id];
+					if(campaigns[i].meta.token.name == "SATTBEP20") {
+						campaigns[i].meta.token.name ="SATT";
+					}
+				}
+
+				var result = await ctr.methods.campaigns(campaigns[i].id).call();
+				campaigns[i].funds =  result.funds;
+				campaigns[i].nbProms =  result.nbProms;
+				campaigns[i].nbValidProms =  result.nbValidProms;
+				campaigns[i].startDate = result.startDate;
+				campaigns[i].endDate = result.endDate;
+
+				var ratios = await ctr.methods.getRatios(campaigns[i].id).call();
+				var types = ratios[0];
+				var likes = ratios[1];
+				var shares = ratios[2];
+				var views = ratios[3];
+				var res = [
+					{typeSN:types[0],likeRatio:likes[0],shareRatio:shares[0],viewRatio:views[0]},
+					{typeSN:types[1],likeRatio:likes[1],shareRatio:shares[1],viewRatio:views[1]},
+					{typeSN:types[2],likeRatio:likes[2],shareRatio:shares[2],viewRatio:views[2]},
+					{typeSN:types[3],likeRatio:likes[3],shareRatio:shares[3],viewRatio:views[3]}];
+				campaigns[i].ratios = res;
+
+				var idproms = await ctr.methods.getProms(campaigns[i].id).call();
+				campaigns[i].proms =[];
+				for (var j =0;j<idproms.length;j++)
+				{
+					var prom = await ctr.methods.proms(idproms[j]).call();
+					prom.id = idproms[j];
+					if(prom.influencer.toLowerCase() == owner.toLowerCase())
+						campaigns[i].proms.push(prom);
+				}
+
+				rescampaigns.push(campaigns[i]);
+			}
+			var campaignscentral = await app.statcentral.campaignsByOwner(owner);
+	        let created_campaigns=rescampaigns.concat(campaignscentral)
+			let auth = await app.crm.auth(access_token);
+			let draft_campaigns = await app.db.campaignCrm().find({idNode:"0"+auth.id,hash:{ $exists: false}}).toArray();
+            draft_campaigns=draft_campaigns.map((c)=>{
+				return {...c,stat:'draft'}
+			})
+
+            let campaigns_=[...created_campaigns,...draft_campaigns]
+			response.end(JSON.stringify(campaigns_));
+
+		}catch(err){
+			response.end('{"error":"'+(err.message?err.message:err.error)+'"}');
+		}
+	})
+
+
+
 
 	app.get('/proms/owner/:owner', async function(req, response) {
 		var owner = req.params.owner;
