1caf51fde043044c84820874da8162c4d49cb023
alaa-lassoued <alaa@atayen.us>
2021-04-22 14:47:58 +0100

    save

--- express/stats.js
+++ express/stats.js
@@ -165,10 +165,11 @@ module.exports = function (app) {
 		var campaignscentral = await app.statcentral.campaignsByInfluencer(address);
 
 		rescampaigns = rescampaigns.concat(campaignscentral);
+
 		let Ended_c=0
 		let Pending_c=0
 
-		var unowned = rescampaigns.filter((campaign) => address?.toLowerCase() !== campaign.owner.toLowerCase())
+		var unowned = [...rescampaigns].filter((campaign) => address?.toLowerCase() !== campaign.owner.toLowerCase())
         
 		for(var c=0;c<unowned.length;c++){
 
@@ -184,7 +185,7 @@ module.exports = function (app) {
 				Ended_c++
 			  }
 		}
-        console.log(Pending_c,Ended_c)
+		
 		response.end(JSON.stringify({allCampaign:rescampaigns,ended:Ended_c,pending:Pending_c}));
 	})
 
