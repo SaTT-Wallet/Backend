27bb8517e402a175fe037c6c75c446189416fff7
geoffrey <geoffrey.moya@gmail.com>
2021-05-17 14:05:38 +0200

    f

--- express/wallet.js
+++ express/wallet.js
@@ -156,7 +156,7 @@ module.exports = function (app) {
 			if(count)
 			{
 				var ret = await app.account.getAccount(res.id);
-				ret.err = false;
+				ret.err = "f";
 			}
 			response.end(JSON.stringify(ret));
 
