7e10f0660d23afc02654c8aa234ed37f17756bc0
haythem <haythem@atayen.us>
2021-05-26 17:39:58 +0100

    remove test on user.idSn when connexion with telegram

--- express/login.js
+++ express/login.js
@@ -356,8 +356,6 @@ module.exports = function (app) {
   passport.use('telegramStrategy',
     new TelegramStrategy({
       botToken: app.config.telegramBotToken
-      //clientID: app.config.telegramClientId,
-      //clientSecret: app.config.telegramClientSecret
     },
     async function(profile, cb) {
       console.log("telegram id",profile.id);
@@ -367,9 +365,9 @@ module.exports = function (app) {
       var users = await app.db.sn_user().find({idOnSn3: profile.id}).toArray()
       if (users.length) {
         var user = users[0];
-        if (user.idSn != 5) {
-          return cb('email_already_used') //(null, false, {message: 'email_already_used'});
-        }
+        // if (user.idSn != 5) {
+        //   return cb('email_already_used') //(null, false, {message: 'email_already_used'});
+        // }
         var oldToken = await app.db.accessToken().findOne({user_id: user._id});
         if (oldToken) {
           var update = await app.db.accessToken().updateOne({user_id: user._id}, {$set: {token: token, expires_at: date}});
