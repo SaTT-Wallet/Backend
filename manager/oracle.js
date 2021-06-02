885ff3c654ed315dd49a9b2378d626c47274efe9
geoffrey <geoffrey.moya@gmail.com>
2021-06-02 14:44:00 +0200

    tokens pages long term

--- manager/oracle.js
+++ manager/oracle.js
@@ -21,7 +21,7 @@ module.exports = async function (app) {
 
 	oracleManager.facebookAbos = async function (pageName,idPost) {
 		return new Promise(async (resolve, reject) => {
-				res2 = await app.db.query("Select pu.token as token from fb_page_admin pa,fb_user_token pu,fb_page_fb pf where pf.id = pa.page_fb and pa.user = pu.user and  pf.name = '"+pageName+"'");
+				res2 = await app.db.query("Select pt.token as token from fb_page_token pt,fb_page_fb pf where pf.id = pt.page and  pf.name = '"+pageName+"'");
 				if(res2 && res2.length) {
 					var token = res2[0].token;
 				var res = await rp({uri:"https://graph.facebook.com/"+app.config.fbGraphVersion+"/"+pageName+"?access_token="+token+"&fields=fan_count",json: true});
@@ -46,7 +46,7 @@ module.exports = async function (app) {
 		return new Promise(async (resolve, reject) => {
 			var res = await rp({uri:"https://api.instagram.com/oembed/?url=https://www.instagram.com/p/"+idPost+"/",json: true});
 			var username = res.author_name;
-			res2 = await app.db.query("Select pi.instagram_id as igid,pu.token as token from fb_page_instagram pi,fb_page_admin pa,fb_user_token pu where pi.page_fb = pa.page_fb and pa.user = pu.user and  pi.username = '"+username+"'");
+			res2 = await app.db.query("Select pi.instagram_id as igid,pt.token as token from fb_page_instagram pi,fb_page_token pt where pi.page_fb = pt.page and  pi.username = '"+username+"'");
 			if(res2 && res2.length) {
 				var ig = res2[0].igid;
 				var token = res2[0].token;
@@ -73,15 +73,16 @@ module.exports = async function (app) {
 
 
 
-	oracleManager.facebookAlt = async function (pageName,idPost) {
+	oracleManager.facebook = async function (pageName,idPost) {
 		return new Promise(async (resolve, reject) => {
-			//app.FB.api(pageName,{ access_token: "" }, async function (res) {
+			res = await app.db.query("Select pt.token as token pf.page_id as page_id from fb_page_token pt,fb_page_fb pf where pf.id = pt.page  and  pf.name = '"+pageName+"'");
+			if(res && res.length) {
+				var token = res[0].token;
+				var idPage = res[0].page_id;
 
-				var res = await rp({uri:"https://graph.facebook.com/v3.2/"+pageName+"?access_token="+app.FB.appAccessToken,json: true});
-				//console.log("pageres",res);
-				var idPage = res.id;
+
 				//console.log("https://graph.facebook.com/v3.2/"+idPage+"_"+idPost+"?fields=shares,likes.summary(true)&access_token="+app.FB.appAccessToken);
-				var res2 = await rp({uri:"https://graph.facebook.com/v3.2/"+idPage+"_"+idPost+"?fields=shares,likes.summary(true)&access_token="+app.FB.appAccessToken,json: true});
+				var res2 = await rp({uri:"https://graph.facebook.com/"+app.config.fbGraphVersion+"/"+idPage+"_"+idPost+"?fields=shares,likes.summary(true)&access_token="+token,json: true});
 
 				//app.FB.api(idPage+"_"+idPost+"?fields=shares,likes.summary(true)",{ access_token: app.FB.appAccessToken }, async function (res2) {
 					var shares = 0;
@@ -96,36 +97,18 @@ module.exports = async function (app) {
 						shares = res2.shares.count;
 					}
 					var perf = {shares:shares,likes:res2.likes.summary.total_count,views:0,date:Math.floor(Date.now()/1000)};
-					app.web3.eth.accounts.wallet.decrypt([app.campaignWallet], app.config.campaignOwnerPass);
-					var h1 = app.web3.utils.sha3(app.web3.eth.abi.encodeParameters(['uint8','string','string','uint64','uint64','uint64','uint64'],[1,idPost,"",perf.shares,perf.likes,perf.views,perf.date]));
-					var sign = await app.web3.eth.sign(h1,app.config.campaignOwner);
-					perf.r = sign.r;
-					perf.s = sign.s;
-					perf.v = sign.v;
+
 					resolve(perf);
-				//});
-			//})
+				}
+
+				else {
+					resolve({shares:0,likes:0,views:0});
+			}
+
 		})
 	};
 
-	oracleManager.facebook = async function (pageName,idPost) {
-		return new Promise(async (resolve, reject) => {
 
-			//var res = await rp({uri:"http://geoffreymoya.hopto.org:3014/fb/page/"+pageName+"/"+idPost,json: true})
-   var perf = {shares:0,likes:0,views:0,date:Math.floor(Date.now()/1000)};
-
-					/*var perf = {shares:res.shares,likes:res.likes,views:0,date:Math.floor(Date.now()/1000)};
-					app.web3.eth.accounts.wallet.decrypt([app.campaignWallet], app.config.campaignOwnerPass);
-					var h1 = app.web3.utils.sha3(app.web3.eth.abi.encodeParameters(['uint8','string','string','uint64','uint64','uint64','uint64'],[1,idPost,"",perf.shares,perf.likes,perf.views,perf.date]));
-					var sign = await app.web3.eth.sign(h1,app.config.campaignOwner);
-					perf.r = sign.r;
-					perf.s = sign.s;
-					perf.v = sign.v;*/
-					resolve(perf);
-				//}
-
-		})
-	};
 
 	oracleManager.youtube = async function (idPost) {
 		return new Promise(async (resolve, reject) => {
@@ -134,20 +117,16 @@ module.exports = async function (app) {
 				idPost = idPost.split("&")[0];
 			}
 			var perf = {shares:0,likes:0,views:0};
-		/*	var body = await rp({uri:'https://www.googleapis.com/youtube/v3/videos',qs:{id:idPost,key:app.config.gdataApiKey,part:"statistics"}});
+			var body = await rp({uri:'https://www.googleapis.com/youtube/v3/videos',qs:{id:idPost,key:app.config.gdataApiKey,part:"statistics"}});
 			var res = JSON.parse(body);
 			if(res.items && res.items[0])
 			{
 
 			perf = {shares:res.items[0].statistics.commentCount,likes:res.items[0].statistics.likeCount,views:res.items[0].statistics.viewCount,date:Math.floor(Date.now()/1000)};
 		 }
-		 */
 
-		   /* var h1 = app.web3.utils.sha3(app.web3.eth.abi.encodeParameters(['uint8','string','string','uint64','uint64','uint64','uint64'],[2,idPost,"",perf.shares,perf.likes,perf.views,perf.date]));
-			var sign = await app.web3.eth.sign(h1,app.config.campaignOwner);
-			perf.r = sign.r;
-			perf.s = sign.s;
-			perf.v = sign.v;*/
+
+
 			resolve(perf);
 		})
 
@@ -156,25 +135,35 @@ module.exports = async function (app) {
 	oracleManager.instagram = async function (idPost) {
 		return new Promise(async (resolve, reject) => {
 				var perf = {shares:0,likes:0,views:0};
-		//	var perf = await rp({uri:"http://geoffreymoya.hopto.org:3014/insta/"+idPost,json: true})
+				var res = await rp({uri:"https://api.instagram.com/oembed/?url=https://www.instagram.com/p/"+idPost+"/",json: true});
+				var username = res.author_name;
+				res2 = await app.db.query("Select pi.instagram_id as igid,pt.token as token from fb_page_instagram pi,fb_page_token pt where pi.page_fb = pt.page and  pi.username = '"+username+"'");
+				if(res2 && res2.length) {
+					var ig = res2[0].igid;
+					var token = res2[0].token;
+					var mediaId = false;
+					var cur = await rp({uri:"https://graph.facebook.com/"+app.config.fbGraphVersion+"/"+ig+"/media?access_token="+token+"&fields=permalink,like_count",json: true});
 
-			/*var body = await rp({uri:"https://www.instagram.com/p/"+idPost});
-			var part = body.split('<meta property="og:description" content="')[1];
-			var part2 = part.split(' ');
-			var likes = parseFloat(part2[0]);
-			if( part2[0].indexOf("k")!=-1)
-				likes *= 1000;
-			var shares = parseFloat(part2[2]);
-			if( part2[2].indexOf("k")!=-1)
-				shares *= 1000;*/
-			//app.web3.eth.accounts.wallet.decrypt([app.campaignWallet], app.config.campaignOwnerPass);
-			//var perf = {likes:likes,shares:shares,views:0,date:Math.floor(Date.now()/1000)};
-			/*var h1 = app.web3.utils.sha3(app.web3.eth.abi.encodeParameters(['uint8','string','string','uint64','uint64','uint64','uint64'],[3,idPost,"",perf.shares,perf.likes,perf.views,perf.date]));
-			var sign = await app.web3.eth.sign(h1,app.config.campaignOwner);
-			perf.r = sign.r;
-			perf.s = sign.s;
-			perf.v = sign.v;*/
-			resolve(perf);
+					while (cur.paging.next) {
+						for(var i = 0 ;i<cur.data.length;i++)
+						{
+							if ("https://www.instagram.com/p/"+idPost+"/" == cur.data[i].permalink) {
+								resolve({shares:0,likes:cur.data[i].like_count,views:0})
+								return;
+							}
+						}
+						cur = await rp({uri:cur.paging.next,json: true});
+					}
+
+
+				}
+
+
+
+				else {
+					resolve(perf);
+			}
+
 		})
 
 	};
@@ -184,22 +173,11 @@ module.exports = async function (app) {
 		return new Promise(async (resolve, reject) => {
 
 
-			/*var body = await rp({uri:"https://twitter.com/"+userName+"/status/"+idPost});
-			var parts = body.split('data-tweet-stat-count=');
-			var partT = parts[1];
-			var partL = parts[2];*/
+
 			var res = await tweet.get('statuses/show',{id:idPost});
 
 			var perf = {shares:res.retweet_count,likes:res.favorite_count,views:0,date:Math.floor(Date.now()/1000)};
 
-			//var perf = {shares:partT.split('"')[1],likes:partL.split('"')[1],views:0,date:Math.floor(Date.now()/1000)};
-			//app.web3.eth.accounts.wallet.decrypt([app.campaignWallet], app.config.campaignOwnerPass);
-			//var h1 = app.web3.utils.sha3(app.web3.eth.abi.encodeParameters(['uint8','string','string','uint64','uint64','uint64','uint64'],[4,idPost,userName,perf.shares,perf.likes,perf.views,perf.date]));
-			//console.log(h1,app.config.campaignOwner)
-			/*var sign = await app.web3.eth.sign(h1,app.config.campaignOwner);
-			perf.r = sign.r;
-			perf.s = sign.s;
-			perf.v = sign.v;*/
 
 			resolve(perf);
 		})
