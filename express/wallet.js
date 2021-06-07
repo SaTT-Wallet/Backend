57a16aa0744672987b25d91aacbb5788240479e6
geoffrey <geoffrey.moya@gmail.com>
2021-06-07 12:18:00 +0200

    T

--- express/wallet.js
+++ express/wallet.js
@@ -97,12 +97,12 @@ module.exports = function (app) {
 
 
 		  const user =  await app.db.sn_user().findOne({_id : id});
-		  
+
 		  if(!user.daily){user.daily = []}
 
 
 		   if(user.daily[0]){
-			variation =  app.token.calculateVariation(total_balance.total_balance, user.daily[0].Balance)
+			variation =  app.token.calculateVariation(Total_balance.Total_balance, user.daily[0].Balance)
 		   }
 
 		  if(!user.daily[0] || user.daily[0].convertDate !== today){
@@ -111,7 +111,7 @@ module.exports = function (app) {
 			delete user._id
 			await app.db.sn_user().updateOne({_id : id}, {$set: user});
 		  }
-		
+
 		  res.end(JSON.stringify({Total_balance, variation})).status(201);
 
 		} catch (err) {
@@ -1280,13 +1280,13 @@ module.exports = function (app) {
 		 app.post('/v3/bep20/transfer',async function(req, response) {
 
 			try {
-	
+
 				var token = req.body.access_token;
 				var to = req.body.to;
 				var amount = req.body.amount;
 				var pass = req.body.pass;
 				var res = await app.crm.auth(token);
-	
+
 				var cred = await app.account.unlockBSC(res.id,pass);
 				cred.from_id = res.id;
 				var ret = await app.bep20.transferBEP(to,amount,cred);
@@ -1676,7 +1676,7 @@ app.post('/v2/profile/update', async function(req, response) {
 			let Crypto = await rp(Fetch_crypto_price);
 			const balance = await app.account.getListCryptoByUid(idUser,Crypto);
 			let listOfCrypto = [...new Set(balance.listOfCrypto)];
-			
+
 			res.send(JSON.stringify({listOfCrypto}))
 		}catch (err) {
 		   res.end('{"error":"'+(err.message?err.message:err.error)+'"}');
