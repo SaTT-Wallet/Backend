e6e35d98ac9107d65055bce741172d8580a28f0a
HamdiBenK <hamdi@atayen.us>
2021-06-18 17:39:41 +0100

    new apis change db to store cron oracles data

--- express/stats.js
+++ express/stats.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
 
 const cron =require('node-cron');
+const Big = require('big.js');
 
 cron.schedule('00 01 * * *',  () => {
 	app.account.BalanceUsersStats("daily");
@@ -51,7 +52,7 @@ cron.schedule("03 04 * * 1", () =>{
 		var proms = [];
 
 		var newproms = await app.db.apply().find({idCampaign:idCampaign}).toArray();
-        let rejectedProms = await app.db.CampaignLinkStatistic().find({$and: [ { idCampaign },{status : "rejected"}]}).toArray();
+        let rejectedProms = await app.db.campaign_link().find({$and: [ { idCampaign },{status : "rejected"}]}).toArray();
    
 		if(idproms.length || newproms.length) {
 			var addresses = [];
@@ -701,6 +702,36 @@ cron.schedule("03 04 * * 1", () =>{
 		 }
 	   })
 
+
+	 app.get('/campaign/statistics/:idCampaign', async (req, res)=>{
+		try{
+
+	 const result = await app.db.campaignCrm().findOne({hash: req.params.idCampaign});
+	 const ctr = await app.campaign.getCampaignContract(req.params.idCampaign);
+     const element = await ctr.methods.campaigns(req.params.idCampaign).call()
+	 const toPayBig = new Big(element.funds[1]);
+	 const bgBudget = new Big(result.cost)
+	 const spent =Math.abs(bgBudget.minus(toPayBig))
+		 
+     res.end(JSON.stringify({toPay : element.funds[1] , spent, initialBudget : result.cost}))
+		}catch (err) {
+		 res.end('{"error":"'+(err.message?err.message:err.error)+'"}');
+	  }
+	})
+
+   app.get('/prom/stats/:idProm', async (req, res) => {
+
+	try{
+	   const idProm = req.params.idProm;
+	   const info =  await app.db.campaign_link().findOne({ id_prom : idProm });
+	   const campaign = await app.db.campaignCrm().findOne({hash : info.id_campaign});
+
+	}catch (err) {
+		res.end('{"error":"'+(err.message?err.message:err.error)+'"}');
+
+	 }
+
+   })
 	return app;
 
 }
