c17d1b510fa3061b7b2b821d5a112733bb448b0c
geoffrey <geoffrey.moya@gmail.com>
2021-06-22 17:21:42 +0200

    double

--- web3/campaign.js
+++ web3/campaign.js
@@ -210,20 +210,20 @@ module.exports = async function (app) {
 
 			//var gasPrice = 4000000000;
 
-				var gasPrice = await ctr.getGasPrice();
-			var isDoubled = await ctr.methods.getIsUsed(idCampaign,typeSN,idPost,idUser).call();
-			if(isDoubled)
-			{
-				reject({message:"Link already sent"});
-			}
-			else {
+		//		var gasPrice = await ctr.getGasPrice();
+			//var isDoubled = await ctr.methods.getIsUsed(idCampaign,typeSN,idPost,idUser).call();
+		//	if(isDoubled)
+			//{
+				//reject({message:"Link already sent"});
+		//	}
+		//	else {
 				var receipt = await ctr.methods.applyCampaign(idCampaign,typeSN,idPost,idUser).send({from:credentials.address, gas:gas,gasPrice: gasPrice});
 					resolve({transactionHash:receipt.transactionHash,idCampaign:idCampaign,typeSN:typeSN,idPost:idPost,idUser:idUser,idProm:prom});
 
 				var prom = receipt.events.CampaignApplied.returnValues.prom;
 				//resolve({transactionHash:receipt.transactionHash,idCampaign:idCampaign,typeSN:typeSN,idPost:idPost,idUser:idUser,idProm:prom});
 				console.log(receipt.transactionHash,"confirmed",idCampaign," prom ",prom);
-				}
+			//	}
 			}
 			catch (err)
 			{
