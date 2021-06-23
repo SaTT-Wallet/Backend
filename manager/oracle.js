a51afb9dab1f399404d31f3b5212c516c8e7155d
geoffrey <geoffrey.moya@gmail.com>
2021-06-23 15:35:39 +0200

    log

--- manager/oracle.js
+++ manager/oracle.js
@@ -187,7 +187,7 @@ module.exports = async function (app) {
 			  access_token_key: twitterProfile.access_token_key,
 			  access_token_secret: twitterProfile.access_token_secret
 			});
-			var res = await tweet.get('tweets' ,{ids:idPost,'tweet.fields':"public_metrics"}); //, non_public_metrics
+			var res = await tweet.get('tweets' ,{ids:idPost,'tweet.fields':"public_metrics,non_public_metrics"}); //, non_public_metrics
 			console.log(res);
 			var perf = {shares:res.data[0].public_metrics.retweet_count,likes:res.data[0].public_metrics.like_count,/*views:res.data[0].non_public_metrics.impression_count,*/date:Math.floor(Date.now()/1000)};
 
