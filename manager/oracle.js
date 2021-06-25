fe28835dba8982e0bb9d97a2c8b05a5f9cc4f034
geoffrey <geoffrey.moya@gmail.com>
2021-06-25 10:39:06 +0200

    vrfy

--- manager/oracle.js
+++ manager/oracle.js
@@ -225,10 +225,14 @@ module.exports = async function (app) {
 				var googleProfile = await app.db.googleProfile().findOne({UserId:userId  });
 
 				var res = await rp({uri:'https://www.googleapis.com/youtube/v3/videos',qs:{id:idPost,key:googleProfile.accessToken,part:"snippet"},json: true});
-			
-				var channelId = res.items[0].snippet.channelId;
-				var googleProfile = await app.db.googleProfile().findOne({UserId:userId,channelId:channelId  });
-				resolve(googleProfile);
+				if(res.items) {
+					var channelId = res.items[0].snippet.channelId;
+					var googleProfile = await app.db.googleProfile().findOne({UserId:userId,channelId:channelId  });
+					resolve(googleProfile);
+			  }
+				else {
+					resolve(false);
+				}
 
 			}catch (err) {
 				reject({message:err.message});
