ef09891f9862fdf25933fc860aa313831428603f
HamdiBenK <hamdi@atayen.us>
2021-06-28 15:14:43 +0100

    progress

--- express/campaign.js
+++ express/campaign.js
@@ -141,13 +141,11 @@ module.exports = function (app) {
 
 	  let updateStat= async ()=>{
 		 console.log("debut de traitement")
-		promDetail=[];
 		let prom;
 		var Events = await app.db.event().find({ prom: { $exists: true} }).toArray();
 		Events.forEach(async (event)=>{
 			var idProm = event.prom;
 			prom = await app.oracle.getPromDetails(idProm)
-			console.log(prom)
 				var stat={};
 
 				stat.status = prom.isAccepted
