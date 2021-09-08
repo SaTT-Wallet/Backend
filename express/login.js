bd87348a46b00750adb62db797baac4277e2af37
khalil Afif <khalil@atayen.us>
2021-09-08 10:41:14 +0100

    add_mail_toLowerCase

--- express/login.js
+++ express/login.js
@@ -1067,7 +1067,7 @@ app.get('/link/twitter/:idUser/:idCampaign', (req, res,next)=>{
   app.post('/auth/passlost', async function (req, response) {
     const lang = req.query.lang || "en";
 
-	app.i18n.configureTranslation(lang);
+	  app.i18n.configureTranslation(lang);
     var mail = req.body.mail;
     // var res = await app.db.query("Select id from user where email='" + mail + "' ");
     var users = await app.db.sn_user().find({email: mail.toLowerCase()}).toArray();
