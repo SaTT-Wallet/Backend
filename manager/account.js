5a5222370100331fd4e2ba6626cc0dedd2fc16c6
HamdiBenK <hamdi@atayen.us>
2021-11-26 11:04:20 +0100

    remove

--- manager/account.js
+++ manager/account.js
@@ -1180,7 +1180,7 @@ accountManager.handleId=async function () {
 		accountManager.sysLogger.log('info',logInfo);	
 	   }
 
-	   accountManager.getPrices = (res)=>{
+	   accountManager.getPrices = ()=>{
 		if(app.prices.status && (Date.now() - (new Date(app.prices.status.timestamp)).getTime() < 1200000)) {
 
 			return app.prices.data;
