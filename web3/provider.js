e8fa11948f601c76a4a6332158535f949ed8abd8
geoffrey <geoffrey.moya@gmail.com>
2021-12-06 15:56:23 +0100

    reconnect options fix

--- web3/provider.js
+++ web3/provider.js
@@ -2,19 +2,30 @@ module.exports = async function (app) {
 	var net = require('net');
 	var Web3 = require('web3');
 
-/*	var options = {
-		  reconnect: {
-			auto: true
-		  }
-	};*/
 
-	const options = {
-  // Enable auto reconnection
-  reconnect: {
-      auto: true,
-      delay: 5000, // ms
-      onTimeout: false
-  }
+
+
+const options = {
+    timeout: 30000, 
+   
+
+    clientConfig: {
+      // Useful if requests are large
+      maxReceivedFrameSize: 100000000,   // bytes - default: 1MiB
+      maxReceivedMessageSize: 100000000, // bytes - default: 8MiB
+
+      // Useful to keep a connection alive
+      keepalive: true,
+      keepaliveInterval: 60000 // ms
+    },
+
+    // Enable auto reconnection
+    reconnect: {
+        auto: true,
+        delay: 5000, // ms
+        maxAttempts: 5,
+        onTimeout: false
+    }
 };
 
 
