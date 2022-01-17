5b3b7cd6311ddd3783759c7107688b7d54755e1a
geoffrey <geoffrey.moya@gmail.com>
2022-01-17 12:55:38 +0100

    eth

--- bridge.js
+++ bridge.js
@@ -68,8 +68,8 @@ try {
     bridge.contractEvtEth = new app.web3.eth.Contract(app.config.ctrs.token.abi,app.config.ctrs.token.address.mainnet);
     bridge.contractEvtBsc = new app.web3Bep20Websocket.eth.Contract(app.config.ctrs.bep20.abi,app.config.ctrs.bep20.address.mainnet);
     
-    bridge.contractEvtBsc.events.Transfer({filter:{to:app.config.SattBep20Addr}},bridge.eventBSCtoETH);
-    //bridge.contractEvtEth.events.Transfer({filter:{to:app.config.SattBep20Addr}},bridge.eventETHtoBSC);
+    //bridge.contractEvtBsc.events.Transfer({filter:{to:app.config.SattBep20Addr}},bridge.eventBSCtoETH);
+    bridge.contractEvtEth.events.Transfer({filter:{to:app.config.SattBep20Addr}},bridge.eventETHtoBSC);
   }
 
   bridge.unlockOwnerBsc = async () => {
