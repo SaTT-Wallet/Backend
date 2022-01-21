1498618d8a77764abbf39d89d5c5fdab8bff7943
Raja Jlidi <raja@atayen.us>
2022-01-21 15:58:49 +0100

    add route controller

---
+++ routes/campaign.routes.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let router = express.Router();
+const {launchCampaign,launchBounty} = require('../controllers/campaign.controller')
+
+
+
+
+router.post('/launch/performance',launchCampaign);
+router.post('/launch/bounty',launchBounty);
+
+
+module.exports = router;
+
