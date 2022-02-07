f843376359d2e9d933f7a0860c94b24d6550b54a
Youssef Benamor <youssef@atayen.us>
2022-02-07 16:13:22 +0100

    new branch first commit

--- model/user.model.js
+++ model/user.model.js
@@ -1,79 +1,74 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  
-    first_name: { type: String},
-    name: { type: String},
-   idOnSn2: { type: String},
-   email: { type: String},
-   username: { type: String},
-   firstName: { type: String},
-   lastName:{ type: String},
-  
-   idSn:{ type: Number},
-   newsLetter: { type: Boolean},
-   onBoarding: { type: Boolean},
-   account_locked:  { type: Boolean},
-   failed_count:{ type: Number},
-   enabled: { type: Number},
-   locale: { type: String},
-   confirmation_token: { type: String},
-   userSatt: { type: Boolean},
-   picLink: { type: String},
-   completed:  { type: Boolean},
-   password: { type: String},
-   secureCode: {
-     code: { type: Number},
-     expiring: { type: Number},
-     type:{ type: String},
+const userSchema = mongoose.Schema(
+  {
+    first_name: { type: String },
+    name: { type: String },
+    idOnSn2: { type: String },
+    email: { type: String },
+    username: { type: String },
+    firstName: { type: String },
+    lastName: { type: String },
+
+    idSn: { type: Number },
+    newsLetter: { type: Boolean },
+    onBoarding: { type: Boolean },
+    account_locked: { type: Boolean },
+    failed_count: { type: Number },
+    enabled: { type: Number },
+    locale: { type: String },
+    confirmation_token: { type: String },
+    userSatt: { type: Boolean },
+    picLink: { type: String },
+    completed: { type: Boolean },
+    password: { type: String },
+    secureCode: {
+      code: { type: Number },
+      expiring: { type: Number },
+      type: { type: String },
+    },
+    new: { type: Boolean },
+    passphrase: { type: Boolean },
+    photoUpdated: { type: Boolean },
+    birthday: { type: String },
+    gender: { type: String },
+
+    daily: [
+      {
+        Date: { type: Number },
+        Balance: { type: String },
+        convertDate: { type: String },
+      },
+    ],
+    address: { type: String },
+    city: { type: String },
+    country: { type: String },
+    zipCode: { type: String },
+    visitPassphrase: { type: Boolean },
+
+    fireBaseAccessToken: { type: String },
+
+    phone: {
+      number: { type: String },
+      internationalNumber: { type: String },
+      nationalNumber: { type: String },
+      e164Number: { type: String },
+      countryCode: { type: String },
+      dialCode: { type: String },
+    },
+
+    daily: [
+      {
+        Date: { type: Number },
+        Balance: { type: String },
+        convertDate: { type: String },
+      },
+    ],
+
+    idOnSn3: { type: String },
+    isChanged: { type: Boolean },
   },
-   new:  { type: Boolean},
-   passphrase:  { type: Boolean},
-   photoUpdated: { type: Boolean},
-   birthday: { type: String},
-   gender: { type: String},
-  
-   daily: [
-     {
-       Date: { type: Number},
-       Balance:{ type: String},
-       convertDate: { type: String},
-    }
-  ],
-   address: { type: String},
-   city: { type: String},
-   country: { type: String},
-   zipCode:{ type: String},
-   visitPassphrase: { type: Boolean},
-  
-   fireBaseAccessToken: { type: String},
-  
-   phone: {
-     number: { type: String},
-     internationalNumber: { type: String},
-     nationalNumber: { type: String},
-     e164Number:{ type: String},
-     countryCode: { type: String},
-     dialCode:{ type: String},
-  },
-   
-   daily: [
-     {
-       Date: { type: Number},
-       Balance:{ type: String},
-       convertDate: { type: String},
-    }
-  ],
+  { timestamps: true, strict: false }
+);
 
-  idOnSn3: { type: String},
-   isChanged:  { type: Boolean},
-  
-  
- 
-  
-
-
-
-},{timestamps:true,strict: false} );
-
-const User = mongoose.model("sn_users", userSchema);
+const User = mongoose.model('sn_users', userSchema);
