148b26a393bf9207988c224b7935179252d3bd92
Youssef Benamor <youssef@atayen.us>
2022-02-10 17:52:15 +0100

    profile.controller refactoring v2

--- model/interests.model.js
+++ model/interests.model.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
+const db = mongoose.createConnection(process.env.MONGOURI)
 
-const interestsSchema = mongoose.Schema({
-    interests: [{ type: String }],
-})
+const interestsSchema = mongoose.Schema(
+    {
+        interests: [{ type: String }],
+        userId: { type: Number },
+    },
+    { collection: 'interests' }
+)
 
-const Interests = mongoose.model('interests', interestsSchema)
+const Interests = db.model('interests', interestsSchema)
+module.exports = Interests
