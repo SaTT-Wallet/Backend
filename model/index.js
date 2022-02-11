489b9dc026e6ce8f401cbde6431270272208e663
Youssef Benamor <youssef@atayen.us>
2022-02-11 15:26:11 +0100

    profile.controller refactoring

--- model/index.js
+++ model/index.js
@@ -3,5 +3,13 @@ const GoogleProfile = require('../model/googleProfile.model')
 const FbProfile = require('../model/fbProfile.model')
 const LinkedinProfile = require('../model/linkedinProfile.model')
 const Interests = require('../model/interests.model')
+const TwitterProfile = require('../model/twitterProfile.model')
 
-module.exports = { User, GoogleProfile, FbProfile, LinkedinProfile, Interests }
+module.exports = {
+    User,
+    GoogleProfile,
+    FbProfile,
+    LinkedinProfile,
+    Interests,
+    TwitterProfile,
+}
