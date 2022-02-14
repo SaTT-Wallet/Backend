2b2270d7a5198640fbc06684513c6fe7dd0d751e
Raja Jlidi <raja@atayen.us>
2022-02-14 11:53:07 +0100

    wallet integration mongoose

--- model/passwallet.model.js
+++ model/passwallet.model.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
+const db = mongoose.createConnection(process.env.MONGOURI)
 
-const passwalletSchema = mongoose.Schema({
-    UserId: { type: Number, required: true, ref: 'sn_users' },
-    value: { type: String },
-})
+const passwalletSchema = mongoose.Schema(
+    {
+        UserId: { type: Number, required: true, ref: 'sn_users' },
+        value: { type: String },
+    },
+    {
+        collection: 'sn_user',
+    }
+)
 
-const PassWallet = mongoose.model('temp', passwalletSchema)
+const PassWallet = db.model('temp', passwalletSchema)
+module.exports = PassWallet
