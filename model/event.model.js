1bf331ec408f8dcff020129fa5fdf9e2c5c7c0d5
haythem <haythem@atayen.us>
2022-02-18 14:40:42 +0100

    fix campaign_controller

--- model/event.model.js
+++ model/event.model.js
@@ -4,17 +4,14 @@ const db = mongoose.createConnection(process.env.MONGOURI)
 const eventSchema = mongoose.Schema(
     {
         id: { type: String },
-
         prom: { type: String },
         type: { type: String },
         date: { type: Number },
         txhash: { type: String },
         contract: { type: String },
-        owner: { type: String },
+        owner: { type: String }
     },
-    {
-        collection: 'event',
-    }
+    { timestamps: true, strict: false,  collection: 'event'}
 )
 
 const Event = db.model('event', eventSchema)
