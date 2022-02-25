df333ced358eaa8188a59fbce34c9fbd64b62119
Raja Jlidi <raja@atayen.us>
2022-02-25 14:25:37 +0100

    changeschages

--- web3/wallets.js
+++ web3/wallets.js
@@ -2,9 +2,11 @@ const { Wallet, CustomToken } = require('../model/index')
 const { responseHandler } = require('../helpers/response-handler')
 const { erc20Connexion, bep20Connexion } = require('../blockchainConnexion')
 
+var rp = require('request-promise')
+const Big = require('big.js')
+
 const { Constants } = require('../conf/const2')
-const { Tokens } = require('../conf/config1').getToken
-console.log(Tokens)
+const { Tokens, token200 } = require('../conf/config1')
 exports.unlock = async (req, res) => {
     try {
         let UserId = req.user._id
@@ -142,91 +144,143 @@ exports.getAccount = async (req, res) => {
     }
 }
 
-exports.getPrices = () => {
-    if (
-        app.prices.status &&
-        Date.now() - new Date(app.prices.status.timestamp).getTime() < 1200000
-    ) {
-        return app.prices.data
-    } else {
-        var r = child.execSync(
-            'curl "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=200&convert=USD&CMC_PRO_API_KEY=' +
-                app.config.cmcApiKey +
-                '"'
-        )
-        var response = JSON.parse(r)
-        var r2 = child.execSync(
-            'curl "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=SATT%2CJET&convert=USD&CMC_PRO_API_KEY=' +
-                app.config.cmcApiKey +
-                '"'
-        )
-        var responseSattJet = JSON.parse(r2)
-        response.data.push(responseSattJet.data.SATT)
-        response.data.push(responseSattJet.data.JET)
+exports.getPrices = async () => {
+    var options = {
+        method: 'GET',
+        uri:
+            'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=200&convert=USD&CMC_PRO_API_KEY=' +
+            process.env.CMCAPIKEY,
 
-        var priceMap = response.data.map((elem) => {
-            var obj = {}
-            obj = {
-                symbol: elem.symbol,
-                name: elem.name,
-                price: elem.quote.USD.price,
-                percent_change_24h: elem.quote.USD.percent_change_24h,
-                market_cap: elem.quote.USD.market_cap,
-                volume_24h: elem.quote.USD.volume_24h,
-                circulating_supply: elem.circulating_supply,
-                total_supply: elem.total_supply,
-                max_supply: elem.max_supply,
-                logo:
-                    'https://s2.coinmarketcap.com/static/img/coins/128x128/' +
-                    elem.id +
-                    '.png',
-            }
-            return obj
-        })
-        var finalMap = {}
-        for (var i = 0; i < priceMap.length; i++) {
-            finalMap[priceMap[i].symbol] = priceMap[i]
-            delete finalMap[priceMap[i].symbol].symbol
+        json: true,
+    }
+
+    var options2 = {
+        method: 'GET',
+        uri:
+            'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=SATT%2CJET&convert=USD&CMC_PRO_API_KEY=' +
+            process.env.CMCAPIKEY,
+
+        json: true,
+    }
+
+    var result = await rp(options)
+    var response = result
+
+    var result2 = await rp(options2)
+    var responseSattJet = result2
+
+    response.data.push(responseSattJet.data.SATT)
+    response.data.push(responseSattJet.data.JET)
+
+    var priceMap = response.data.map((elem) => {
+        var obj = {}
+        obj = {
+            symbol: elem.symbol,
+            name: elem.name,
+            price: elem.quote.USD.price,
+            percent_change_24h: elem.quote.USD.percent_change_24h,
+            market_cap: elem.quote.USD.market_cap,
+            volume_24h: elem.quote.USD.volume_24h,
+            circulating_supply: elem.circulating_supply,
+            total_supply: elem.total_supply,
+            max_supply: elem.max_supply,
+            logo:
+                'https://s2.coinmarketcap.com/static/img/coins/128x128/' +
+                elem.id +
+                '.png',
         }
 
-        for (var i = 0; i < app.config.token200.length; i++) {
-            var token = app.config.token200[i]
-            if (finalMap[token.symbol]) {
-                finalMap[token.symbol].network = token.platform.network
-                finalMap[token.symbol].tokenAddress =
-                    token.platform.token_address
-                finalMap[token.symbol].decimals = token.platform.decimals
-            }
+        return obj
+    })
+    var finalMap = {}
+    for (var i = 0; i < priceMap.length; i++) {
+        finalMap[priceMap[i].symbol] = priceMap[i]
+        delete finalMap[priceMap[i].symbol].symbol
+    }
+
+    for (var i = 0; i < token200.length; i++) {
+        var token = token200[i]
+
+        if (finalMap[token.symbol]) {
+            finalMap[token.symbol].network = token.platform.network
+            finalMap[token.symbol].tokenAddress = token.platform.token_address
+            finalMap[token.symbol].decimals = token.platform.decimals
         }
     }
+
     response.data = finalMap
-    app.prices = response
 
     return finalMap
 }
 
+exports.filterAmount = function (input, nbre = 10) {
+    if (input) {
+        var out = input
+        let size = input.length
+        let toAdd = parseInt(nbre) - parseInt(size)
+
+        if (input == 0) {
+            toAdd--
+        }
+        if (toAdd > 0) {
+            if (input.includes('.')) {
+                for (let i = 0; i < toAdd; i++) {
+                    out += '0'
+                }
+            } else {
+                out += '.'
+                for (let i = 0; i < toAdd; i++) {
+                    out += '0'
+                }
+            }
+        } else if (toAdd < 0) {
+            if (input.includes('.')) {
+                if (input.split('.')[0].length > nbre) {
+                    out = input.substring(0, nbre)
+                } else {
+                    out = input.substring(0, nbre)
+                    if (out[nbre - 1] == '.') {
+                        out = input.substring(0, nbre - 1)
+                    }
+                }
+            }
+        }
+
+        return out
+    } else {
+        return '-'
+    }
+}
+
+exports.getBalance = async (Web3, token, address) => {
+    try {
+        let contract = new Web3.eth.Contract(Constants.token.abi, token)
+        amount = await contract.methods.balanceOf(address).call()
+
+        return amount.toString()
+    } catch (err) {
+        return '0'
+    }
+}
+
 exports.getListCryptoByUid = async (req, res) => {
     let id = req.user._id
-    let crypto = this.getPrices()
+    let crypto = await this.getPrices()
+    var listOfCrypto = []
     try {
-        console.log('start')
-
-        console.log(app.config.Tokens)
-        let listOfCrypto = []
-        var token_info = Object.assign({}, app.config.Tokens)
-        let sattContract = token_info['SATT'].contract
+        var token_info = Object.assign({}, Tokens)
         delete token_info['SATT']
         delete token_info['BNB']
         var CryptoPrices = crypto
 
         var ret = await this.getAccount(req, res)
-
         delete ret.btc
         delete ret.version
 
         let userTokens = await CustomToken.find({
             sn_users: { $in: [id] },
         })
+
         if (userTokens.length) {
             for (let i = 0; i < userTokens.length; i++) {
                 let symbol = userTokens[i].symbol
@@ -259,11 +313,23 @@ exports.getListCryptoByUid = async (req, res) => {
             crypto.undername2 = token_info[T_name].undername2
             ;[crypto.price, crypto.total_balance] = Array(2).fill(0.0)
 
-            let networkToken = network == 'ERC20' ? app.erc20 : app.bep20
-            let balance = await networkToken.getBalance(
-                token_info[T_name].contract,
-                ret.address
-            )
+            let Web3ETH = await erc20Connexion()
+            let Web3BEP20 = await bep20Connexion()
+
+            let balance = {}
+            if (network == 'ERC20') {
+                balance.amount = await this.getBalance(
+                    Web3ETH,
+                    token_info[T_name].contract,
+                    ret.address
+                )
+            } else {
+                balance.amount = await this.getBalance(
+                    Web3BEP20,
+                    token_info[T_name].contract,
+                    ret.address
+                )
+            }
 
             let key = T_name.split('_')[0]
 
@@ -279,7 +345,7 @@ exports.getListCryptoByUid = async (req, res) => {
                 crypto.price = CryptoPrices[key].price
                 crypto.variation = CryptoPrices[key].percent_change_24h
                 crypto.total_balance =
-                    app.token.filterAmount(
+                    this.filterAmount(
                         new Big(balance['amount'])
                             .div((10 ** +token_info[T_name].dicimal).toString())
                             .toNumber() + ''
@@ -287,7 +353,7 @@ exports.getListCryptoByUid = async (req, res) => {
                     CryptoPrices[key].price *
                     1
             }
-            crypto.quantity = app.token.filterAmount(
+            crypto.quantity = this.filterAmount(
                 new Big(balance['amount'] * 1)
                     .div((10 ** +token_info[T_name].dicimal).toString())
                     .toNumber()
@@ -295,6 +361,7 @@ exports.getListCryptoByUid = async (req, res) => {
 
             listOfCrypto.push(crypto)
         }
+
         delete ret.address
         for (const Amount in ret) {
             let crypto = {}
@@ -312,7 +379,7 @@ exports.getListCryptoByUid = async (req, res) => {
             if (tokenSymbol == 'SATT') {
                 crypto.name = 'SaTT'
                 crypto.network = 'ERC20'
-                crypto.contract = sattContract
+                crypto.contract = Constants.token.satt
             } else if (tokenSymbol == 'BNB') {
                 crypto.name = 'BNB'
                 crypto.network = 'BEP20'
@@ -321,27 +388,128 @@ exports.getListCryptoByUid = async (req, res) => {
                 Array(3).fill(tokenSymbol)
             crypto.price = CryptoPrices[tokenSymbol].price
             crypto.variation = CryptoPrices[tokenSymbol].percent_change_24h
+
             crypto.total_balance =
-                app.token.filterAmount(
-                    new Big(ret[Amount])
+                this.filterAmount(
+                    new Big(await ret[Amount])
                         .div(new Big(10).pow(decimal))
                         .toNumber() + ''
                 ) * CryptoPrices[tokenSymbol].price
-            crypto.quantity = new Big(ret[Amount])
+            crypto.quantity = new Big(await ret[Amount])
                 .div(new Big(10).pow(decimal))
                 .toNumber()
                 .toFixed(8)
             listOfCrypto.push(crypto)
         }
 
-        console.log(listOfCrypto)
         return { listOfCrypto }
     } catch (err) {
+        return responseHandler.makeResponseError(
+            res,
+            500,
+            err.message ? err.message : err.error
+        )
+    }
+}
+
+exports.getBalanceByUid = async (req, res) => {
+    try {
+        var userId = req.user._id
+        let crypto = await this.getPrices()
+
+        var [Total_balance, CryptoPrices] = [0, crypto]
+        var token_info = Object.assign({}, Tokens)
+        delete token_info['SATT']
+        delete token_info['BNB']
+
+        let ret = await this.getAccount(req, res)
+        delete ret.btc
+        delete ret.version
+
+        let userTokens = await CustomToken.find({
+            sn_users: { $in: [userId] },
+        })
+
+        if (userTokens.length) {
+            for (let i = 0; i < userTokens.length; i++) {
+                let symbol = userTokens[i].symbol
+                if (token_info[symbol])
+                    symbol = `${symbol}_${userTokens[i].network}`
+                token_info[symbol] = {
+                    dicimal: Number(userTokens[i].decimal),
+                    symbol: userTokens[i].symbol,
+                    network: userTokens[i].network,
+                    contract: userTokens[i].tokenAdress,
+                    name: userTokens[i].tokenName,
+                    picUrl: userTokens[i].picUrl,
+                    addedToken: true,
+                }
+            }
+        }
+
+        for (const T_name in token_info) {
+            var network = token_info[T_name].network
+
+            let Web3ETH = await erc20Connexion()
+            let Web3BEP20 = await bep20Connexion()
+
+            let balance = {}
+            if (network == 'ERC20') {
+                balance.amount = await this.getBalance(
+                    Web3ETH,
+                    token_info[T_name].contract,
+                    ret.address
+                )
+            } else {
+                balance.amount = await this.getBalance(
+                    Web3BEP20,
+                    token_info[T_name].contract,
+                    ret.address
+                )
+            }
+
+            let key = T_name.split('_')[0]
+            if (
+                token_info[T_name].contract ==
+                    token_info['SATT_BEP20'].contract ||
+                token_info[T_name].contract == token_info['WSATT'].contract
+            ) {
+                key = 'SATT'
+            }
+            if (CryptoPrices.hasOwnProperty(key)) {
+                Total_balance +=
+                    this.filterAmount(
+                        new Big(balance['amount'] * 1)
+                            .div((10 ** +token_info[T_name].dicimal).toString())
+                            .toNumber() + ''
+                    ) * CryptoPrices[key].price
+            }
+        }
+
+        delete ret.address
+        for (const Amount in ret) {
+            let tokenSymbol = Amount.split('_')[0].toUpperCase()
+            tokenSymbol = tokenSymbol === 'ETHER' ? 'ETH' : tokenSymbol
+
+            let decimal = tokenSymbol === 'BTC' ? 8 : 18
+
+            Total_balance +=
+                this.filterAmount(
+                    new Big((await ret[Amount]) * 1)
+                        .div(new Big(10).pow(decimal))
+                        .toNumber() + ''
+                ) * CryptoPrices[tokenSymbol].price
+        }
+
+        Total_balance = Total_balance.toFixed(2)
+
+        return { Total_balance }
+    } catch (err) {
         console.log(err)
-        //        return responseHandler.makeResponseError(
-        //     res,
-        //     500,
-        //     err.message ? err.message : err.error
-        // )
+        //    return responseHandler.makeResponseError(
+        // 		 res,
+        // 		 500,
+        // 		 err.message ? err.message : err.error
+        // 		 )
     }
 }
