b381ee9e1421a369fc1d143bfc465ebfba7fcd87
Raja Jlidi <raja@atayen.us>
2022-03-02 16:56:37 +0100

    add config

--- conf/settings.js
+++ conf/settings.js
@@ -6,7 +6,13 @@ let environment = {
         MONGO_PASS: process.env.MONGO_PASS_LOCAL,
         MONGO_HOST: process.env.MONGO_HOST_LOCAL,
         MONGO_PORT: process.env.MONGO_PORT_LOCAL,
-        MONGOURI: process.env.MONGOURI_LOCAL,
+        MONGOURI:
+            'mongodb://' +
+            process.env.MONGO_HOST_LOCAL +
+            ':' +
+            process.env.MONGO_PORT_LOCAL +
+            '/' +
+            process.env.MONGO_BASE_LOCAL,
     },
     testnet: {
         MONGO_BASE: process.env.MONGO_BASE_TESTNET,
@@ -32,7 +38,17 @@ let environment = {
         MONGO_PASS: process.env.MONGO_PASS_MAINNET,
         MONGO_HOST: process.env.MONGO_HOST_MAINNET,
         MONGO_PORT: process.env.MONGO_PORT_MAINNET,
-        //  MONGOURI: process.env.MONGOURI_MAINNET,
+        MONGOURI:
+            'mongodb://' +
+            process.env.MONGO_USER_TESTNET +
+            ':' +
+            process.env.MONGO_PASS_TESTNET +
+            '@' +
+            process.env.MONGO_HOST_TESTNET +
+            ':' +
+            process.env.MONGO_PORT_TESTNET +
+            '/' +
+            process.env.MONGO_BASE_TESTNET,
     },
 }
 
