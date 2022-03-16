79f510a14f3b58229059655a46761944c2a851cd
Raja Jlidi <raja@atayen.us>
2022-03-16 16:02:45 +0100

    update app

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.locals.error = req.app.get('env') === 'testnet' ? err : {}
 
     // render the error page
     res.status(err.status || 500)
