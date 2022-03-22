6de1e117c2a58812d7001925b377bfe59daa0a2c
haythem <haythem@atayen.us>
2022-03-22 12:54:20 +0100

    add redirection to plateform when access denied(twitter,facebook,linkedin,youtube)

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -172,7 +172,6 @@ exports.updateProfile = async (req, res) => {
         const id = req.user._id
         let profile = req.body
 
-        console.log(profile)
         if (profile.email) {
             const user = await User.findOne({
                 $and: [{ email: profile.email }, { _id: { $nin: [id] } }],
