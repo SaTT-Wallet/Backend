b7ffaa072d52789d00906883208a968e2a8868f9
haythem <haythem@atayen.us>
2022-03-23 16:38:50 +0100

    add cache getPrice

--- web3/wallets.js
+++ web3/wallets.js
@@ -1,6 +1,7 @@
 const { Wallet, CustomToken } = require('../model/index')
 const { responseHandler } = require('../helpers/response-handler')
 const { erc20Connexion, bep20Connexion } = require('../blockchainConnexion')
+var cache = require('memory-cache')
 
 var rp = require('request-promise')
 const Big = require('big.js')
@@ -179,82 +180,81 @@ exports.getAccount = async (req, res) => {
     }
 }
 
-// exports.getPrices = async () => {
-//     try {
-//         var prices = null
-//         if (!prices) {
-//             var options = {
-//                 method: 'GET',
-//                 uri:
-//                     'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=200&convert=USD&CMC_PRO_API_KEY=' +
-//                     process.env.CMCAPIKEY,
+exports.getPrices = async () => {
+    try {
+        if (
+            cache.get('prices') &&
+            Date.now() - new Date(cache.get('prices')?.date).getTime() < 1200000
+        ) {
+            return cache.get('prices').data
+        } else {
+            var options = {
+                method: 'GET',
+                uri:
+                    'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=200&convert=USD&CMC_PRO_API_KEY=' +
+                    process.env.CMCAPIKEY,
 
-//                 json: true,
-//             }
+                json: true,
+            }
 
-//             var options2 = {
-//                 method: 'GET',
-//                 uri:
-//                     'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=SATT%2CJET&convert=USD&CMC_PRO_API_KEY=' +
-//                     process.env.CMCAPIKEY,
+            var options2 = {
+                method: 'GET',
+                uri:
+                    'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=SATT%2CJET&convert=USD&CMC_PRO_API_KEY=' +
+                    process.env.CMCAPIKEY,
 
-//                 json: true,
-//             }
-//             let result = await Promise.all([rp(options), rp(options2)])
-//             var response = result[0]
-//             var responseSattJet = result[1]
-//             response.data.push(responseSattJet.data.SATT)
-//             response.data.push(responseSattJet.data.JET)
+                json: true,
+            }
+            let result = await Promise.all([rp(options), rp(options2)])
+            var response = result[0]
+            var responseSattJet = result[1]
+            response.data.push(responseSattJet.data.SATT)
+            response.data.push(responseSattJet.data.JET)
 
-//             var priceMap = response.data.map((elem) => {
-//                 var obj = {}
-//                 obj = {
-//                     symbol: elem.symbol,
-//                     name: elem.name,
-//                     price: elem.quote.USD.price,
-//                     percent_change_24h: elem.quote.USD.percent_change_24h,
-//                     market_cap: elem.quote.USD.market_cap,
-//                     volume_24h: elem.quote.USD.volume_24h,
-//                     circulating_supply: elem.circulating_supply,
-//                     total_supply: elem.total_supply,
-//                     max_supply: elem.max_supply,
-//                     logo:
-//                         'https://s2.coinmarketcap.com/static/img/coins/128x128/' +
-//                         elem.id +
-//                         '.png',
-//                 }
+            var priceMap = response.data.map((elem) => {
+                var obj = {}
+                obj = {
+                    symbol: elem.symbol,
+                    name: elem.name,
+                    price: elem.quote.USD.price,
+                    percent_change_24h: elem.quote.USD.percent_change_24h,
+                    market_cap: elem.quote.USD.market_cap,
+                    volume_24h: elem.quote.USD.volume_24h,
+                    circulating_supply: elem.circulating_supply,
+                    total_supply: elem.total_supply,
+                    max_supply: elem.max_supply,
+                    logo:
+                        'https://s2.coinmarketcap.com/static/img/coins/128x128/' +
+                        elem.id +
+                        '.png',
+                }
 
-//                 return obj
-//             })
-//             var finalMap = {}
-//             for (var i = 0; i < priceMap.length; i++) {
-//                 finalMap[priceMap[i].symbol] = priceMap[i]
-//                 delete finalMap[priceMap[i].symbol].symbol
-//             }
+                return obj
+            })
+            var finalMap = {}
+            for (var i = 0; i < priceMap.length; i++) {
+                finalMap[priceMap[i].symbol] = priceMap[i]
+                delete finalMap[priceMap[i].symbol].symbol
+            }
 
-//             for (var i = 0; i < token200.length; i++) {
-//                 var token = token200[i]
+            for (var i = 0; i < token200.length; i++) {
+                var token = token200[i]
 
-//                 if (finalMap[token.symbol]) {
-//                     finalMap[token.symbol].network = token.platform.network
-//                     finalMap[token.symbol].tokenAddress =
-//                         token.platform.token_address
-//                     finalMap[token.symbol].decimals = token.platform.decimals
-//                 }
-//             }
-
-//             response.data = finalMap
-//             prices = response
-
-//             return finalMap
-//         } else if (
-//             prices.status &&
-//             Date.now() - new Date(prices.status.timestamp).getTime() < 1200000
-//         ) {
-//             return prices.data
-//         }
-//     } catch (err) {}
-// }
+                if (finalMap[token.symbol]) {
+                    finalMap[token.symbol].network = token.platform.network
+                    finalMap[token.symbol].tokenAddress =
+                        token.platform.token_address
+                    finalMap[token.symbol].decimals = token.platform.decimals
+                }
+            }
+            prices = { data: finalMap, date: Date.now() }
+            cache.put('prices', prices)
+            return finalMap
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 exports.filterAmount = function (input, nbre = 10) {
     if (input) {
