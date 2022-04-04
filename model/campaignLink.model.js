d3099f9df0ab40c841adf81dc5b3220f52707388
Raja Jlidi <raja@atayen.us>
2022-04-04 14:58:41 +0100

    gains

--- model/campaignLink.model.js
+++ model/campaignLink.model.js
@@ -21,7 +21,6 @@ const campaignLinkSchema = mongoose.Schema(
         views: { type: Number, default: 0 },
         totalToEarn: { type: String },
         payedAmount: { type: String },
-        lastHarvestDate: { type: mongoose.Schema.Types.Mixed },
     },
     {
         collection: 'campaign_link',
