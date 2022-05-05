bd02369b6bdb708f922a3a27c7aa0c56d7d447a8
haythem <haythem@atayen.us>
2022-05-05 12:29:10 +0100

    fix gasprice bep20 connexion

--- controllers/wallet.controller.js
+++ controllers/wallet.controller.js
@@ -166,7 +166,7 @@ exports.gasPricePolygon = async (req, res) => {
 }
 
 exports.gasPriceBep20 = async (req, res) => {
-    let Web3ETH = await erc20Connexion()
+    let Web3ETH = await bep20Connexion()
 
     var gasPrice = await Web3ETH.eth.getGasPrice()
     return responseHandler.makeResponseData(res, 200, 'success', {
