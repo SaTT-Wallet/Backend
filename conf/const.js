018a9d58e70dc673aee12f12fc98faca4b99547e
waelhanfi04 <wael@atayen.us>
2022-07-19 17:25:48 +0100

    wbtt

--- conf/const.js
+++ conf/const.js
@@ -1,5 +1,6 @@
 let Constants = {
-    token: { abi: [], satt: '', tether: '', dai: '' },
+    token: { abi: [], satt: '', tether: '', dai: '', wbtt: '' },
+    wbtt: { abi: [] },
     campaign: {
         abi: [],
         address: { campaignErc20: '', campaignBep20: '', campaignPolygon: '' },
@@ -1605,6 +1606,188 @@ Constants.wSaTT.abi = [
         stateMutability: 'nonpayable',
         type: 'function',
     },
+]
+
+Constants.wbtt.abi = [
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'src',
+                type: 'address',
+            },
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'guy',
+                type: 'address',
+            },
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'wad',
+                type: 'uint256',
+            },
+        ],
+        name: 'Approval',
+        type: 'event',
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'dst',
+                type: 'address',
+            },
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'wad',
+                type: 'uint256',
+            },
+        ],
+        name: 'Deposit',
+        type: 'event',
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'src',
+                type: 'address',
+            },
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'dst',
+                type: 'address',
+            },
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'wad',
+                type: 'uint256',
+            },
+        ],
+        name: 'Transfer',
+        type: 'event',
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'src',
+                type: 'address',
+            },
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'wad',
+                type: 'uint256',
+            },
+        ],
+        name: 'Withdrawal',
+        type: 'event',
+    },
+    {
+        inputs: [
+            { internalType: 'address', name: '', type: 'address' },
+            { internalType: 'address', name: '', type: 'address' },
+        ],
+        name: 'allowance',
+        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        inputs: [
+            { internalType: 'address', name: 'guy', type: 'address' },
+            { internalType: 'uint256', name: 'wad', type: 'uint256' },
+        ],
+        name: 'approve',
+        outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        inputs: [{ internalType: 'address', name: '', type: 'address' }],
+        name: 'balanceOf',
+        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        inputs: [],
+        name: 'decimals',
+        outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        inputs: [],
+        name: 'deposit',
+        outputs: [],
+        stateMutability: 'payable',
+        type: 'function',
+    },
+    {
+        inputs: [],
+        name: 'name',
+        outputs: [{ internalType: 'string', name: '', type: 'string' }],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        inputs: [],
+        name: 'symbol',
+        outputs: [{ internalType: 'string', name: '', type: 'string' }],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        inputs: [],
+        name: 'totalSupply',
+        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        inputs: [
+            { internalType: 'address', name: 'dst', type: 'address' },
+            { internalType: 'uint256', name: 'wad', type: 'uint256' },
+        ],
+        name: 'transfer',
+        outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        inputs: [
+            { internalType: 'address', name: 'src', type: 'address' },
+            { internalType: 'address', name: 'dst', type: 'address' },
+            { internalType: 'uint256', name: 'wad', type: 'uint256' },
+        ],
+        name: 'transferFrom',
+        outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        inputs: [{ internalType: 'uint256', name: 'wad', type: 'uint256' }],
+        name: 'withdraw',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    { stateMutability: 'payable', type: 'receive' },
 ]
 Constants.priceGap.abi = [
     {
@@ -3369,6 +3552,8 @@ if (process.env.NODE_ENV === 'mainnet') {
     BttConstants.oracle.address = process.env.CONST_ORACLE_ADDRESS_MAINNET_BTT
 } else {
     Constants.token.satt = process.env.CONST_TOKEN_ADDRESS_TESTNET
+    Constants.token.wbtt = "0xd6cb96a00b312d5930fc2e8084a98ff2daa5ad2e"
+
     Constants.token.tether = process.env.CONST_TOKEN_ADDRESS_TETHERTESTNET
     Constants.token.dai = process.env.CONST_TOKEN_ADDRESS_DAITESTNET
     Constants.campaign.address.campaignErc20 =
@@ -3413,7 +3598,7 @@ let bep20TokenCampaigns = [
 
 let polygonTokensCampaign = [PolygonConstants.token.satt.toLowerCase()]
 
-let bttTokensCampaign = [BttConstants.token.satt.toLowerCase()]
+let bttTokensCampaign = [BttConstants.token.satt.toLowerCase(),  Constants.token.wbtt.toLowerCase()]
 
 exports.web3UrlBep20 = process.env.WEB3_URL_BEP20
 exports.web3UrlBTT = process.env.WEB3_URL_BTT
