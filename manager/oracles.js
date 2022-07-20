9a77a2ba2b84cf0391d71e8ccfe9d91b0347dfc3
Youssef <youssef@atayen.us>
2022-07-20 15:30:31 +0100

    update 2

--- manager/oracles.js
+++ manager/oracles.js
@@ -532,7 +532,7 @@ exports.getPromApplyStats = async (
         delete socialOracle.date
         return socialOracle
     } catch (err) {
-        console.log('err from getPromApplyStats', err.message)
+        console.log('error from getPromApplyStats', err.message)
     }
 }
 
