bdda82ef2c0b69aedb0c9661934a3d64004b8ae7
geoffrey <geoffrey.moya@gmail.com>
2022-08-31 18:02:22 +0200

    options

--- web3/web3-connection.js
+++ web3/web3-connection.js
@@ -1,10 +1,39 @@
 const Web3 = require('web3')
 
+const options = {
+    timeout: 30000,
+
+    clientConfig: {
+        // Useful if requests are large
+        maxReceivedFrameSize: 100000000, // bytes - default: 1MiB
+        maxReceivedMessageSize: 100000000, // bytes - default: 8MiB
+
+        // Useful to keep a connection alive
+        keepalive: true,
+        keepaliveInterval: 60000, // ms
+    },
+
+    // Enable auto reconnection
+    reconnect: {
+        auto: true,
+        delay: 5000, // ms
+        maxAttempts: 5,
+        onTimeout: false,
+    },
+}
+
 const networkProviders = {
     ETH: process.env.WEB3_URL,
     BSC: process.env.WEB3_URL_BEP20,
     POLYGON: process.env.WEB3_URL_POLYGON,
     BTTC: process.env.WEB3_URL_BTT,
+}
+
+const networkProvidersOptions = {
+    ETH: options,
+    BSC: options,
+    POLYGON: options,
+    BTTC: options,
 }
 
 const getWeb3Connection = function (provider) {
@@ -19,4 +48,5 @@ module.exports = {
     getWeb3Connection,
     getHttpProvider,
     networkProviders,
+    networkProvidersOptions,
 }
