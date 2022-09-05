38c13fde2512d1adaffc642f4578859439487690
waelhanfi04 <wael@atayen.us>
2022-09-05 11:30:24 +0100

    fix slow tiktok abos

--- manager/oracles.js
+++ manager/oracles.js
@@ -487,6 +487,7 @@ exports.tiktokAbos = async (username) => {
 
 
     const page = await browser.newPage()
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36')
     await page.goto(vgmUrl)
     const scrappedData = await page.$$eval('strong', (elements) => {
         return elements
