69eff347e22984d9e0d7ba12eb29c79355c05466
Youssef <youssef@atayen.us>
2022-10-03 09:49:16 +0100

    add youtube stats

--- libs/statistic-social-network/index.js
+++ libs/statistic-social-network/index.js
@@ -1,7 +1,9 @@
 const { twitter } = require('./twitter')
 const { tiktok } = require('./tiktok')
+const { youtube } = require('./youtube')
 
 module.exports = {
     twitter,
     tiktok,
+    youtube,
 }
---
+++ libs/statistic-social-network/youtube.js
@@ -0,0 +1,33 @@
+module.exports.youtube = async (idPost) => {
+    try {
+        if (idPost.indexOf('&') !== -1) {
+            idPost = idPost.split('&')[0]
+        }
+        var perf = { shares: 0, likes: 0, views: 0, media_url: '' }
+        var body = await rp({
+            uri: 'https://www.googleapis.com/youtube/v3/videos',
+            qs: {
+                id: idPost,
+                key: oauth.google.gdataApiKey,
+                part: 'statistics',
+            },
+        })
+        var res = JSON.parse(body)
+        var media = await rp({
+            uri: `https://www.youtube.com/oembed?url=https%3A//youtube.com/watch%3Fv%3D${idPost}&format=json`,
+            json: true,
+        })
+        if (res.items && res.items[0]) {
+            perf = {
+                shares: 0 /*res.items[0].statistics.commentCount*/,
+                likes: res.items[0].statistics.likeCount,
+                views: res.items[0].statistics.viewCount,
+                date: Math.floor(Date.now() / 1000),
+                media_url: media?.thumbnail_url || ' ',
+            }
+        }
+        return perf
+    } catch (err) {
+        console.log(err.message)
+    }
+}
