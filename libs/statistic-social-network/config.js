ee201cf0888ad149fed77fb3b348417d1410283f
Youssef <youssef@atayen.us>
2022-10-04 17:20:00 +0100

    some changes

--- libs/statistic-social-network/config.js
+++ libs/statistic-social-network/config.js
@@ -1,13 +1,8 @@
 module.exports.config = {
-    LINKEDIN_FIRST_URL_ADRR_FIRST: 'test',
-    LINKEDIN_FIRST_URL_ADRR_SECOND: '',
     LINKEDIN_SHARE_FIRST:
         'https://api.linkedin.com/v2/shares?ids=urn:li:share:',
     LINKEDIN_START_URL_FIRST:
         'https://api.linkedin.com/v2/organizationalEntityShareStatistics?q=organizationalEntity&',
     LINKEDIN_START_URL_SECOND: 's[0]=urn:li:',
     LINKEDIN_START_URL_THIRD: '&organizationalEntity=',
-    LINKEDIN_UGC_POST_FIRST: '',
-    LINKEDIN_UGC_POST_SECOND: '',
-    LINKEDIN_ORIGINAL_ARTICLE: '',
 }
--- libs/statistic-social-network/helpers.js
+++ libs/statistic-social-network/helpers.js
@@ -1,24 +1,7 @@
 const { config } = require('./config')
 
 module.exports = {
-    linkedinActivityUrl: (activityURN) =>
-        `${config.LINKEDIN_FIRST_URL_ADRR_FIRST}${activityURN}${config.LINKEDIN_FIRST_URL_ADRR_SECOND}`,
     linkedinShareUrl: (shareURN) => `${config.LINKEDIN_SHARE_FIRST}${shareURN}`,
     linkedinStatsUrl: (type, idPost, organization) =>
         `${config.LINKEDIN_START_URL_FIRST}${type}${config.LINKEDIN_START_URL_SECOND}${type}:${idPost}${config.LINKEDIN_START_URL_THIRD}${organization}`,
-    linkedinUgcPostStats: (idPost) =>
-        `${config.LINKEDIN_UGC_POST_FIRST}${idPost}${config.LINKEDIN_UGC_POST_SECOND}`,
-    linkedinPages: (accessToken) => {
-        return {
-            url: 'https://api.linkedin.com/v2/organizationAcls?q=roleAssignee&projection=(elements*(*, organization~(localizedName,logoV2(original~:playableStreams))))',
-            method: 'GET',
-            headers: {
-                Authorization: 'Bearer ' + accessToken,
-                'X-Restli-Protocol-Version': '2.0.0',
-            },
-            json: true,
-        }
-    },
-    linkedinMediaUrl: (idPost) =>
-        `${config.LINKEDIN_ORIGINAL_ARTICLE}${idPost}`,
 }
