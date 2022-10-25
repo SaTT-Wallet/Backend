5b64e087a450d41d7e7bd248e5e965d346962d2c
Raja Jlidi <raja@atayen.us>
2022-10-25 18:06:50 +0100

    up

--- middleware/passport.middleware.js
+++ middleware/passport.middleware.js
@@ -634,7 +634,7 @@ exports.twitterAuthSignup = async (
 
     console.log('profile..................;', profile)
     //var date = Math.floor(Date.now() / 1000) + 86400
-    let user = await User.findOneOrCreate({ idOnSn: profile.id })
+    let user = await User.findOne({ idOnSn: profile.id })
 
     if (user) {
         await handleSocialMediaSignin({ idOnSn2: profile.id }, cb)
