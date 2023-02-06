14d3ea7a7b7186fc53ef39238955930745381abe
HamdiBenK <hamdi@atayen.us>
2023-02-06 10:34:21 +0100

    transfer tron

--- controllers/wallet.controller.js
+++ controllers/wallet.controller.js
@@ -1066,3 +1066,41 @@ exports.countWallets = async (req, res) => {
 
     return responseHandler.makeResponseData(res, 200, 'success', countWallets)
 }
+
+exports.transfertAllTron = async (req, res) => {
+    try {
+        const userId = req.user._id
+        const { pass } = req.body
+        // Check user migration
+        if (req.user?.migrate)
+            return responseHandler.makeResponseData(
+                res,
+                401,
+                'success',
+                'migration already done'
+            )
+
+        //const BEP20 = await bep20Connexion();
+        const accountData = await Wallet.findOne({ UserId: userId })
+        if (accountData) {
+            let tronWeb = await webTronInstance()
+            let privateKey = (await getWalletTron(userId, pass)).priv
+            let amount = await tronWeb.trx.getBalance(accountData.tronAddress)
+
+            result = await transferTronTokens({
+                tronAddress: accountData.tronAddress,
+                toAddress: to /* accountData.walletV2.tronAddress*/,
+                amount,
+                privateKey,
+            })
+
+            return res.json({ transactionHash: result.transactionHash })
+        }
+    } catch (err) {
+        return responseHandler.makeResponseError(
+            res,
+            500,
+            err.message ? err.message : err.error
+        )
+    }
+}
