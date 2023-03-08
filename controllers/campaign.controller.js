a1c26877b06536af662993aa8242eece1fc17260
Raja Jlidi <raja@atayen.us>
2023-03-08 10:59:47 +0100

    issue display campaignLink

--- controllers/campaign.controller.js
+++ controllers/campaign.controller.js
@@ -2281,9 +2281,18 @@ exports.getLinks = async (req, res) => {
         let userLinks = await CampaignLink.aggregate([
             {
                 $match: {
-                    id_campaign: {
-                        $in: [query1.id_campaign, query3.id_campaign],
-                    },
+                    $or: [
+                        {
+                            id_wallet: {
+                                $in: [query1.id_wallet, query3.id_wallet],
+                            },
+                        },
+                        {
+                            id_campaign: {
+                                $in: [query1.id_campaign, query3.id_campaign],
+                            },
+                        },
+                    ],
                 },
             },
             {
