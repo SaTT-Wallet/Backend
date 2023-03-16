591fc35c0c519767f11b901417eeeae135ec5fd4
HamdiBenK <hamdi@atayen.us>
2023-03-16 16:15:40 +0100

    change CORS

--- app.js
+++ app.js
@@ -84,11 +84,9 @@ app.use(cors('*'));
   app.use((req, res, next) => {
     if (process.env.NODE_ENV === 'mainnet') {
     if(req.headers.origin) {
-        if(req.headers.origin === "https://dapp.satt.com") return next();
-        return res.redirect("https://satt-token.com");
+        if(req.headers.origin === "https://dapp.satt.com" || req.headers.origin === "https://satt-token.com" || req.headers.origin === "https://app.ihave.io" || req.headers.origin === "http://backoffice.atayen.us") return next();
     } else {
         if(req.url.includes('google') || req.url.includes('facebook') || req.url.includes('tiktok') || req.url.includes('linkedin') || req.url.includes('twitter') || req.url.includes('telegram')) return next();
-        return res.redirect("https://satt-token.com");
     }
 }
  return next();
