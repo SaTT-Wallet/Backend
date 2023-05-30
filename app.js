664fae9bf9ac563df752b363730c7b84eb8d6fc7
hichri-louay <louay@satt.com>
2023-05-30 10:53:03 +0100

    remove commented-out code

--- app.js
+++ app.js
@@ -69,37 +69,6 @@ app.use(helmet.frameguard({ action: 'deny' }));
 
 app.use(cors('*'))
 app.use(corsSetup);
-/*app.use((req, res, next) => {
-    if (process.env.NODE_ENV == "mainnet") {
-        if (req.headers.origin) {
-            if (
-                req.headers.origin === 'https://dapp.satt.com' ||
-                req.headers.origin === 'https://satt-token.com' ||
-                req.headers.origin === 'https://app.ihave.io' ||
-                req.headers.origin === 'http://backoffice.atayen.us'
-            ) {
-                return next()
-            } else return res.redirect("https://satt-token.com");
-            
-                
-        } else {
-            if (
-                req.url.includes('google') ||
-                req.url.includes('facebook') ||
-                req.url.includes('tiktok') ||
-                req.url.includes('linkedin') ||
-                req.url.includes('twitter') ||
-                req.url.includes('telegram')
-            ) {
-                return next()
-            } else return res.redirect("https://satt-token.com");
-            
-                
-                
-        }
-    } else return next()
-    
-})*/
 
 app.use(logger('combined'))
 app.use(express.json())
