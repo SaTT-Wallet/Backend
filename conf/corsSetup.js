1a40980e312d6f4287c5e0eedf3adc95314b6478
hichri-louay <louay@satt.com>
2023-07-17 16:21:30 +0100

    add api key for mwf

--- conf/corsSetup.js
+++ conf/corsSetup.js
@@ -1,35 +1,41 @@
 require('dotenv').config()
 const corsSetup = (req, res, next) =>{
-    if (process.env.NODE_ENV == "mainnet") {
-        if (req.headers.origin) {
-            if (
-                req.headers.origin === 'https://dapp.satt.com' ||
-                req.headers.origin === 'https://satt-token.com' ||
-                req.headers.origin === 'https://app.ihave.io' ||
-                req.headers.origin === 'http://backoffice.atayen.us'
-            ) {
-                return next()
-            } else return res.redirect("https://satt-token.com");
-            
+    const apiKey = req.headers['api-key'];
+    if(apiKey && apiKey === process.env.API_KEY) {
+        return next();
+    } else {
+        if (process.env.NODE_ENV == "mainnet") {
+            if (req.headers.origin) {
+                if (
+                    req.headers.origin === 'https://dapp.satt.com' ||
+                    req.headers.origin === 'https://satt-token.com' ||
+                    req.headers.origin === 'https://app.ihave.io' ||
+                    req.headers.origin === 'http://backoffice.atayen.us'
+                ) {
+                    return next()
+                } else return res.redirect("https://satt-token.com");
                 
-        } else {
-            if (
-                req.url.includes('google') ||
-                req.url.includes('youtube') ||
-                req.url.includes('facebook') ||
-                req.url.includes('tikTok') ||
-                req.url.includes('tiktok') ||
-                req.url.includes('linkedin') ||
-                req.url.includes('twitter') ||
-                req.url.includes('telegram')
-            ) {
-                return next()
-            } else return res.redirect("https://satt-token.com");
-            
+                    
+            } else {
+                if (
+                    req.url.includes('google') ||
+                    req.url.includes('youtube') ||
+                    req.url.includes('facebook') ||
+                    req.url.includes('tikTok') ||
+                    req.url.includes('tiktok') ||
+                    req.url.includes('linkedin') ||
+                    req.url.includes('twitter') ||
+                    req.url.includes('telegram')
+                ) {
+                    return next()
+                } else return res.redirect("https://satt-token.com");
                 
-                
-        }
-    } else return next()
+                    
+                    
+            }
+        } else return next()
+    }
+    
 }
 
 
