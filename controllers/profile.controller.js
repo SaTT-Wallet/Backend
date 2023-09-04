41e5c7727f4442b08b47fab161ce3381e2b15706
hichri-louay <louay@satt.com>
2023-09-04 15:12:39 +0100

    wip

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -904,7 +904,7 @@ module.exports.getNotifications = async (req, res) => {
                 { applyerSignature: 0 } // Exclude unnecessary fields from the result
               );
           
-              if (link && link.type === 'waiting_for_validation') {
+              if (link) {
                 notification.label.linkExist = true;
                 notification.label.link = link;
               } else notification.label.linkExist = false;
